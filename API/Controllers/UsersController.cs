75b19872cce20ebe52f78d7d3a87a08ba49733b0
Achyut <achyutp10@gmail.com>
2025-02-18 09:11:21 +0545

    Get user/s

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] // /api/users
+    [ApiController]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")] // /api/users/3
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = context.Users.Find(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(user);
+            }
+
+        }
+    }
+}
