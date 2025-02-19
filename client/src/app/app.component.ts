cb76eb35ee9a0379c5af84aaa59aa6b4c3a4a284
Achyut <achyutp10@gmail.com>
2025-02-19 13:04:49 +0545

    home page and register form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ import { AccountService } from './_services/account.service';
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NavComponent]
+  imports: [NavComponent, HomeComponent]
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
