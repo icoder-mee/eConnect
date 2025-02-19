cd8df947025e3180611ce0bc5d418b86997f76e5
Achyut <achyutp10@gmail.com>
2025-02-19 16:41:28 +0545

    toastr for toast notification

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, Input, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +11,8 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+    private toastr = inject(ToastrService);
+
   // @Input() userFromHomeComponent: any;
   // userFromHomeComponent = input.required<any>()
   // @Output() cancelRegister = new EventEmitter();
@@ -22,7 +25,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error),
+      error: error => this.toastr.error(error.error),
     })
   }
 
