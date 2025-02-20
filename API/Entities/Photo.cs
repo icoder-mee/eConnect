d6a7e333a1bb6afe66e6917d6c18ed8c04ee5538
Achyut <achyutp10@gmail.com>
2025-02-20 09:45:59 +0545

    navigation property & seeding data

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,18 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public required string Url { get; set; }
         public bool IsMain { get; set; }
         public string? PublicId { get; set; }
+
+        // Navigation Properties
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; } = null!;
     }
 }
