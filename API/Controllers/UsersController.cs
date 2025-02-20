bed2e8e4938244f1924fa736ab5756c9d2b7644c
Achyut <achyutp10@gmail.com>
2025-02-20 18:29:29 +0545

    update

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers;
 //{
@@ -14,7 +15,7 @@ namespace API.Controllers;
 //    [ApiController]
 
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -33,5 +34,23 @@ namespace API.Controllers;
            return user;  
 
         }
-    }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+         var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+         if (username == null) return BadRequest("Could not find user");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return BadRequest("Coudnt find user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
+        }
+}
 
