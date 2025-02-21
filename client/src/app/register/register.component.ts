32c848c1f57ae28d9867717f73e2b8015463b838
Achyut <achyutp10@gmail.com>
2025-02-21 19:29:23 +0545

    Reactive Forms and client side validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,32 +1,48 @@
-import { Component, EventEmitter, inject, input, Input, output, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, inject, input, Input, OnInit, output, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  // imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
-    private toastr = inject(ToastrService);
+  private toastr = inject(ToastrService);
 
   // @Input() userFromHomeComponent: any;
   // userFromHomeComponent = input.required<any>()
   // @Output() cancelRegister = new EventEmitter();
   cancelRegister = output<boolean>();
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+    })
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error),
-    })
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => this.toastr.error(error.error),
+    // })
   }
 
   cancel() {
