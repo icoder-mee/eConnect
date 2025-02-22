3c20871045b8ddadab53133f521a53c1805d67ce
Achyut <achyutp10@gmail.com>
2025-02-22 13:17:44 +0545

    ngx pagination component

--- API/Extensions/HttpExtensions.cs
+++ API/Extensions/HttpExtensions.cs
@@ -10,7 +10,7 @@ namespace API.Extensions
             var paginationHeader = new PaginationHeader(data.CurrentPage, data.PageSize, data.TotalCount, data.TotalPages);
             var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
             response.Headers.Append("Pagination", JsonSerializer.Serialize(paginationHeader, jsonOptions));
-            response.Headers.Append("Acces-Control-Expose-Headers", "Pagination");
+            response.Headers.Append("Access-Control-Expose-Headers", "Pagination");
 
 
         }
