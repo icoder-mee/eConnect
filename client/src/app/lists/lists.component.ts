f1f7d132be92704e389ea1583c539cca6160011e
Achyut <achyutp10@gmail.com>
2025-03-07 18:47:09 +0545

    paginating on client

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -4,17 +4,19 @@ import { Member } from '../_models/member';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { FormsModule } from '@angular/forms';
 import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
-  imports: [ButtonsModule, FormsModule, MemberCardComponent],
+  imports: [ButtonsModule, FormsModule, MemberCardComponent, PaginationModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
 export class ListsComponent implements OnInit {
-private likesService = inject(LikesService);
-members: Member[] = [];
+likesService = inject(LikesService);
 predicate = 'liked'
+pageNumber = 1;
+pageSize = 5;
 
 ngOnInit(): void {
   this.loadLikes();
@@ -29,9 +31,14 @@ getTitle() {
 }
 
 loadLikes() {
-  this.likesService.getLikes(this.predicate).subscribe({
-    next: members => this.members = members
-  })
+  this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize);
+}
+
+pageChanged(event: any) {
+  if (this.pageNumber !== event.page) {
+    this.pageNumber = event.page;
+    this.loadLikes();
+  }
 }
 
 }
