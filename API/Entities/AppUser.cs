8485710e9a981f6f1035633accd579b12a53bf96
Achyut <achyutp10@gmail.com>
2025-03-07 12:00:21 +0545

    like entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -29,5 +29,8 @@ namespace API.Entities
         //    return DateOfBirth.CalculateAge();
         //}
 
+        public List<UserLike> LikedByUsers { get; set; } = [];
+        public List<UserLike> LikedUsers { get; set; } = [];
+
     }
 }
