4d779cda82e04fac3953e84682ca7e123aeee24e
Achyut <achyutp10@gmail.com>
2025-03-17 15:51:48 +0545

    setting entity for identity and role management

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -25,8 +25,7 @@ namespace API.Controllers
 
             var user = mapper.Map<AppUser>(registerDto);
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
+           
 
             //var user = new AppUser
             //{
@@ -61,15 +60,7 @@ namespace API.Controllers
             {
                 return Unauthorized("Invalid usernames");
             }
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
-
-            }
+            
             return new UserDto
             {
                 Username = user.UserName,
