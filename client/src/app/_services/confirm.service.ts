02872b7e70e8efbe5b65d03ff9548718f84dc256
Achyut <achyutp10@gmail.com>
2025-03-20 15:30:51 +0545

    added confirm service

--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
-// import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
 import { map } from 'rxjs';
 
 @Injectable({
@@ -24,13 +24,13 @@ export class ConfirmService {
         btnCancelText
       }
     };
-    // this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
-    // return this.bsModalRef.onHidden?.pipe(
-    //   map(() => {
-    //     if (this.bsModalRef?.content) {
-    //       return this.bsModalRef.content.result;
-    //     } else return false;
-    //   })
-    // )
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+    return this.bsModalRef.onHidden?.pipe(
+      map(() => {
+        if (this.bsModalRef?.content) {
+          return this.bsModalRef.content.result;
+        } else return false;
+      })
+    )
   }
 }
